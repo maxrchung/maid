848d3229ac5026f965f7965cbb0eebf2dcb41adc
maxrchung <maxrchung@gmail.com>
2026-04-05 02:55:07 -0700

    start osb output

--- maid.py
+++ maid.py
@@ -4,4 +4,17 @@ from mathutils import *
 D = bpy.data
 C = bpy.context
 
-print('hello')
+sb_path = r"C:\Users\Max\AppData\Local\osu!\Songs\beatmap-639109698540463924-a\a - a (S2VX) [a].osb"
+
+with open(sb_path, "w") as file:
+  file.write("[Events]")
+  file.write("//Background and Video events")
+  file.write("//Storyboard Layer 0 (Background)")
+  file.write("//Storyboard Layer 1 (Fail)")
+  file.write("//Storyboard Layer 2 (Pass)")
+  file.write("//Storyboard Layer 3 (Foreground)")
+  file.write("//Storyboard Layer 4 (Overlay)")
+  file.write("//Storyboard Sound Samples")
+
+
+print('Done')
