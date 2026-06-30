386ad970401086569ea3b23a8cc928f4257d271b
maxrchung <maxrchung@gmail.com>
2026-06-29 23:08:41 -0700

    ionno some stuff?

--- script/maid.py
+++ script/maid.py
@@ -21,11 +21,26 @@ importlib.reload(render)
 
 import bpy
 from mathutils import *
-from mathutils.geometry import intersect_tri_tri_2d
+from mathutils.geometry import intersect_point_tri, intersect_tri_tri_2d
 from bpy_extras.object_utils import world_to_camera_view
 from storyboard import Storyboard
 from materials import create_materials
 from render import render_triangle
+
+def is_a_behind_b(a, b):
+    for v in a:
+        p = intersect_point_tri(v, b[0], b[1], b[2])
+
+        if p:
+            return v.z >= p.z
+    
+    for v in b:
+        p = intersect_point_tri(v, b[0], b[1], b[2])
+
+        if p:
+            return v.z <= p.z
+        
+    return None
 
 def order_triangles(triangles):
     # Order in world coordinates using topo sort
@@ -34,34 +49,42 @@ def order_triangles(triangles):
 
     for i in range(len(triangles)):
         for j in range(i + 1, len(triangles)):
-            t1, t2 = triangles[i], triangles[j]
+            a, b = triangles[i], triangles[j]
 
-            if not intersect_tri_tri_2d(t1[0], t1[1], t1[2], t2[0], t2[1], t2[2]):
+            if not intersect_tri_tri_2d(a[0].xy, a[1].xy, a[2].xy, b[0].xy, b[1].xy, b[2].xy):
                 continue
 
-            if t1 > t2
-                graph[t1].append(t2)
-                indegree[t2] = indegree[t2] + 1
+            if is_a_behind_b(a, b):
+                graph[j].append(i)
+                indegree[i] += 1
+            else:
+                graph[i].append(j)
+                indegree[j] += 1
 
-
-    order = []
+    ordered = []
         
-    while indegree
+    while indegree:
         smallest_indegree = min(indegree.values())
             
-        if smallest_indegree > 0
-            there's a loop btw       
-    
-        for key in list(indegree.keys())
-            if indegree[key] > smallest_indegree
+        for key in list(indegree.keys()):
+            if indegree[key] > smallest_indegree:
                 continue
                 
-            order.append(key)
-
-            for dep in graph[key]
-                indegree[dep] = indegree[dep] - 1
-
+            ordered.append(key)
             del indegree[key]
+
+            for dep in graph[key]:
+                if dep not in indegree:
+                    print("3-way cycle")
+                    continue
+
+                indegree[dep] -= 1
+
+            if smallest_indegree > 0:
+                print("smallest_indegree > 0")
+                break
+
+    return ordered
 
 materials = create_materials()
 
@@ -109,8 +132,9 @@ while frame <= frame_end:
 
         world_matrix = object.matrix_world
         world_matrix_3x3 = world_matrix.to_3x3()
+        view_matrix = camera.matrix_world.inverted()
 
-        world_triangles = []
+        camera_triangles = []
         for loop_triangle in mesh.loop_triangles:
             # Cull if normal points away from camera
             world_normal = (world_matrix_3x3 @ loop_triangle.normal).normalized()
@@ -119,32 +143,34 @@ while frame <= frame_end:
                 continue
 
             world_triangle = [world_matrix @ mesh.vertices[i].co for i in loop_triangle.vertices]
-            world_triangles.append(world_triangle)
 
-        ordered_triangles = order_triangles(world_triangles)
-        
-        for world_triangle in ordered_triangles:
             # Bottom left origin where x and y are 0 to 1
             camera_triangle = [world_to_camera_view(scene, camera, v) for v in world_triangle]
 
             # Cull if all behind camera
-            if all(vertex.z <= 0 for vertex in camera_triangle):
+            if all(v.z <= 0 for v in camera_triangle):
                 continue
 
             # Cull if all outside edge
-            if (all(vertex.x >= 1 for vertex in camera_triangle) or
-                all(vertex.x <= 0 for vertex in camera_triangle) or
-                all(vertex.y >= 1 for vertex in camera_triangle) or
-                all(vertex.y <= 0 for vertex in camera_triangle)):
+            if (all(v.x >= 1 for v in camera_triangle) or
+                all(v.x <= 0 for v in camera_triangle) or
+                all(v.y >= 1 for v in camera_triangle) or
+                all(v.y <= 0 for v in camera_triangle)):
                 continue
 
+            camera_triangles.append(camera_triangle)
+
+        # Order back to front
+        ordered_triangles = order_triangles(camera_triangles)
+        
+        for ordered_triangle in ordered_triangles:
             # Transform to osu! coordinates
             osu_triangle = [
                 Vector((
                     v.x * constants.STORYBOARD_SIZE.x + constants.STORYBOARD_OFFSET.x,
                     (1 - v.y) * constants.STORYBOARD_SIZE.y + constants.STORYBOARD_OFFSET.y
                 ))
-                for v in camera_triangle
+                for v in ordered_triangle
             ]
 
             # Get material file
